ee5cde8e641cd708e3a8a567433475e2f0cf24a6
John Ky <newhoggy@gmail.com>
2016-06-16 23:57:47 +1000

    Some extra syntax for type-classes

--- pico-fp/src/main/scala/org/pico/fp/syntax/package.scala
+++ pico-fp/src/main/scala/org/pico/fp/syntax/package.scala
@@ -4,18 +4,83 @@ import scala.language.higherKinds
 
 package object syntax {
   implicit class ApplicativeOps_Gggv37e[A](val self: A) {
-    def point[F[_]](value: A)(implicit ev: Applicative[F]) = ev.point(value)
+    @inline
+    final def point[F[_]](value: A)(implicit ev: Applicative[F]) = ev.point(value)
   }
 
-  implicit class BindOps_Gggv37e[F[_], A](val self: F[A]) {
-    def flatMap[B](f: A => F[B])(implicit ev: Bind[F]) = ev.bind(self)(f)
+  implicit class BindOps_dYso3C7[F[_], A](val self: F[A]) {
+    @inline
+    final def flatMap[B](f: A => F[B])(implicit ev: Bind[F]) = ev.bind(self)(f)
   }
 
-  implicit class ApplyOps_Gggv37e[F[_], A](val self: F[A]) {
+  implicit class FunctorOps_Af6AfhR[F[_], A](val self: F[A]) extends AnyVal {
+    @inline
+    final def map[B](f: A => B)(implicit ev: Functor[F]): F[B] = ev.map(self)(f)
+  }
+
+  implicit class ApplicativeOps_4qFxAUN[A](val self: A) extends AnyVal {
+    @inline
+    final def point[Z[_]](implicit ev: Applicative[Z]): Z[A] = ev.point(self)
+  }
+
+  implicit class ApplyOps_aTbBqLC[F[_], A](val self: F[A]) {
+    @inline
     final def <*>[B](f: F[A => B])(implicit ev: Apply[F]): F[B] = ev.ap(self)(f)
 
+    @inline
     final def *>[B](fb: F[B])(implicit ev: Apply[F]): F[B] = ev.apply2(self,fb)((_,b) => b)
 
+    @inline
     final def <*[B](fb: F[B])(implicit ev: Apply[F]): F[A] = ev.apply2(self,fb)((a,_) => a)
+  }
+
+  implicit class ApplyOps_rbxCYvU[Z[_], A, R](val self: Z[A => R]) extends AnyVal {
+    @inline
+    final def ap(f: Z[A])(implicit ev: Apply[Z]): Z[R] = ev.ap(f)(self)
+  }
+
+  implicit class ApplyOps_4hDXayC[Z[_], A](val self: Z[A]) extends AnyVal {
+    @inline
+    final def applyIn[R](f: A => R)(implicit ev: Apply[Z]): Z[R] = self.map(f)
+  }
+
+  implicit class ApplyOps_8XpxLNu[Z[_], A, B](val self: (Z[A], Z[B])) extends AnyVal {
+    @inline
+    final def applyIn[R](f: A => B => R)(implicit ev: Apply[Z]): Z[R] = self._1.map(f) ap self._2
+
+    @inline
+    final def applyIn[R](f: (A, B) => R)(implicit ev: Apply[Z]): Z[R] = applyIn(f.curried)
+  }
+
+  implicit class ApplyOps_ptXXVAZ[Z[_], A, B, C](val self: (Z[A], Z[B], Z[C])) extends AnyVal {
+    @inline
+    final def applyIn[R](f: A => B => C => R)(implicit ev: Apply[Z]): Z[R] = self._1.map(f) ap self._2 ap self._3
+
+    @inline
+    final def applyIn[R](f: (A, B, C) => R)(implicit ev: Apply[Z]): Z[R] = applyIn(f.curried)
+  }
+
+  implicit class ApplyOps_4xjeQnw[Z[_], A, B, C, D](val self: (Z[A], Z[B], Z[C], Z[D])) extends AnyVal {
+    @inline
+    final def applyIn[R](f: A => B => C => D => R)(implicit ev: Apply[Z]): Z[R] = self._1.map(f) ap self._2 ap self._3 ap self._4
+
+    @inline
+    final def applyIn[R](f: (A, B, C, D) => R)(implicit ev: Apply[Z]): Z[R] = applyIn(f.curried)
+  }
+
+  implicit class ApplyOps_7byu6fr[Z[_], A, B, C, D, E](val self: (Z[A], Z[B], Z[C], Z[D], Z[E])) extends AnyVal {
+    @inline
+    final def applyIn[R](f: A => B => C => D => E => R)(implicit ev: Apply[Z]): Z[R] = self._1.map(f) ap self._2 ap self._3 ap self._4 ap self._5
+
+    @inline
+    final def applyIn[R](f: (A, B, C, D, E) => R)(implicit ev: Apply[Z]): Z[R] = applyIn(f.curried)
+  }
+
+  implicit class ApplyOps_mD3cHW3[Z[_], A, B, C, D, E, F](val self: (Z[A], Z[B], Z[C], Z[D], Z[E], Z[F])) extends AnyVal {
+    @inline
+    final def applyIn[R](f: A => B => C => D => E => F => R)(implicit ev: Apply[Z]): Z[R] = self._1.map(f) ap self._2 ap self._3 ap self._4 ap self._5 ap self._6
+
+    @inline
+    final def applyIn[R](f: (A, B, C, D, E, F) => R)(implicit ev: Apply[Z]): Z[R] = applyIn(f.curried)
   }
 }
